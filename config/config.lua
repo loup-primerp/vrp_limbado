c7c224aaddc316ccb48a7db0270fcf3aec9e63dd
Loup <ocauebarretto@gmail.com>
2021-06-22 08:58:13 -0300

    VRP_LIMBADO

--- config/config.lua
+++ config/config.lua
@@ -5,8 +5,7 @@ config = { }
 
     config.displayText = '~y~Precione ~g~E~s~ ~y~para sair do limbo!'
 	config.key = 38 													
-	config.preset = false											
-	config.coords = { x=0.0, y=0.0, z=0.0 }						
+	config.preset = false															
 	config.z_check = 0.0												
 	config.freeze = false												
 	config.freeze_time = 2											
